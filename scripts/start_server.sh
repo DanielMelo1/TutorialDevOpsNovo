20be93d1dc9235e9437338a26ecc790c33c288c2
Daniel Augusto Reimão Melo <82515273+DanielMelo1@users.noreply.github.com>
2024-11-02 22:51:42 -0300

    atualização

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Define a porta que o servidor usará
+# Define a porta em que a aplicação será executada
 PORT=80
 
-# Verifica se ha processos usando a porta 80 e os mata
+# Verifica se há algum processo rodando na porta especificada e o finaliza
 if lsof -t -i:$PORT > /dev/null; then
-    echo "Matar processos que estão usando a porta $PORT..."
+    echo "Finalizando processos que estão usando a porta $PORT..."
     kill -9 $(lsof -t -i:$PORT)
 else
     echo "Nenhum processo encontrado na porta $PORT."
 fi
 
-# Aguarde um momento para garantir que o processo foi finalizado
+# Aguarda alguns segundos para garantir que o processo foi encerrado
 sleep 2
 
-# Inicie o servidor
+# Inicia a aplicação e direciona os logs para um arquivo
 echo "Iniciando o servidor na porta $PORT..."
-node /home/ec2-user/app/app.js > /home/ec2-user/app/app.log 2>&1 &
+nohup node /home/ec2-user/app/app.js > /home/ec2-user/app/app.log 2>&1 &
+
+# Verifica se a aplicação foi iniciada corretamente
 if [ $? -eq 0 ]; then
-    echo "Servidor iniciado. Verifique os logs em /home/ec2-user/app/app.log."
+    echo "Servidor iniciado com sucesso. Verifique os logs em /home/ec2-user/app/app.log."
 else
     echo "Falha ao iniciar o servidor. Verifique os logs em /home/ec2-user/app/app.log para mais detalhes."
 fi
