83f4767be8590a8a94ac19f9a28cc2ff406595a7
Daniel Augusto Reimão Melo <82515273+DanielMelo1@users.noreply.github.com>
2024-11-03 22:36:59 -0300

    Atualização app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const requestHandler = (request, response) => {
-    response.end('Bem-vindo ao AWS CodePipeline com Node.js!');
+    response.end('Bem-vindo ao AWS CodePipeline com Node.js! VERSÃO NOVAAAA!!!!');
 };
 
 const server = http.createServer(requestHandler);
