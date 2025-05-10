372fa3aeac742989d72a9ff89908d52ecbe2f717
DanielMelo1 <dreimao4@gmail.com>
2025-05-10 01:10:36 -0300

    Ajuste no código main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -184,9 +184,25 @@ resource "aws_iam_instance_profile" "ec2_codedeploy_profile" {
   role = aws_iam_role.ec2_codedeploy_role.name
 }
 
+# Buscar dinamicamente a AMI do Amazon Linux 2023
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 # Instância EC2 na sub-rede pública 2 (us-east-1b)
 resource "aws_instance" "lab_ec2" {
-  ami                    = "ami-0f88e0871fd81e91"  # AMI específica da professora
+  ami                    = "ami-0f88e0871fd81e91"  # AMI específica do Amazon Linux 2023
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_2.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
